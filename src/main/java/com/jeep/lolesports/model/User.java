4035528ccf5e985393f390104fb49639a3a4de0c
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2018-06-20 00:07:06 -0500

    Heroku is not creating a user table

--- src/main/java/com/jeep/lolesports/model/User.java
+++ src/main/java/com/jeep/lolesports/model/User.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Table(name = "UserAppHeroku")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
